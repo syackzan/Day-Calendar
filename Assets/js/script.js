98ef9f0aa38c341a7070a1a380b2381680adeb0d
Scotty Yackzan <syackzan@gmail.com>
2021-08-18 21:23:46 -0700

    Changed row HTML Styling & Added button functionality to Store TextArea Content into Local Storage + have that text displayed when page is reloaded

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,25 @@
-console.log("hello");
+// Storing HTML Elements Into Variables //
+var nineI = $("#nineI");
+var nineB = $("#nineB");
+
+// Variables //
+var nineArr = [];
+var pNineArr = [];
+
+//Getting Stored Values//
+printNine();
+
+// Functions to Store and retrieve into and from local storage //
+function storeNine () {
+    localStorage.setItem("nineam", nineI.val());
+    printNine ();
+}
+
+//Printing Stored Items into Text Area //
+function printNine (){
+    var storedNineX = localStorage.getItem("nineam");
+    nineI.text(storedNineX);
+}
+
+//On click Events//
+nineB.on("click", storeNine);
