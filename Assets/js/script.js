2708d9e298b3fb84f18272281e4f47923086739c
Scotty Yackzan <syackzan@gmail.com>
2021-08-19 22:05:38 -0700

    Added timer and Dynamic Changes to 9:00am row + started adding new rows for other times

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,17 @@
 // Storing HTML Elements Into Variables //
 var nineI = $("#nineI");
 var nineB = $("#nineB");
+var timeDisplay = $("#currentDay");
+
+//Variables to extract HTML for Dynamic Styling TextBox//
+var nineTBG = $("#nineTBG");
+var nineTime = $("#nineTime").text();
+console.log(nineTime);
 
 // Variables //
 var nineArr = [];
 var pNineArr = [];
+var time;
 
 //Getting Stored Values//
 printNine();
@@ -23,3 +30,56 @@ function printNine (){
 
 //On click Events//
 nineB.on("click", storeNine);
+
+//Time Functions//
+var timeReceiver = moment().format("dddd, MMMM Do YYYY, h:mm a");
+timeDisplay.text(timeReceiver);
+
+//Comparison to time//
+
+
+var timeInterval = setInterval(function () {
+  
+}, 60000);
+
+//Repeat Timer function//
+var timeInterval = setInterval(function () {
+    
+    time = moment().format("h")
+    console.log(time);
+    var amPM = moment().format("a");
+    console.log(amPM);
+    
+
+    if ("9" < time || "am" == amPM){
+        nineTBG.addClass("future");
+    } else if ("9" == time || "am" == amPM){
+        nineTBG.removeClass("future");
+        nineTBG.addClass("present");
+    } else {
+        nineTBG.removeClass("present");
+        nineTBG.addClass("past");
+    }
+
+}, 60000);
+
+//Immediately Set Backgrounds when Website fires up//
+function presentTime() {
+    time = moment().format("h")
+    console.log(time);
+    var amPM = moment().format("a");
+    console.log(amPM);
+    
+
+    if ("9" < time || "am" == amPM){
+        nineTBG.addClass("future");
+    } else if ("9" == time || "am" == amPM){
+        nineTBG.removeClass("future");
+        nineTBG.addClass("present");
+    } else {
+        nineTBG.removeClass("present");
+        nineTBG.addClass("past");
+    }
+}
+
+presentTime();
